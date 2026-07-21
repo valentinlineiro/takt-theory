5c58fec641b3347b8cfd9ee1546a84c2fe16ea4c
Valentín Liñeiro <valentinlineiro@gmail.com>
2026-07-21 16:36:34 +0200

    proof(ST-015): formalize A0, T1, and T2 (upset and R_min minimum)

--- takt-formal/TaktFormal/StructuralSufficiency.lean
+++ takt-formal/TaktFormal/StructuralSufficiency.lean
@@ -42,4 +42,114 @@ theorem T4_monotonicity {Z1 Z2 : Type} (R1 : S → Z1) (R2 : S → Z2) (h_refine
 
 end CapabilityTheory
 
+section StructuralSufficiency
 
+variable {S Z A C : Type}
+variable (K : C → S → S → Prop)
+variable (C_D : C → Prop)
+variable (D : S → A)
+
+inductive EqvGen {α : Type} (r : α → α → Prop) : α → α → Prop where
+  | rel   : ∀ (x y : α), r x y → EqvGen r x y
+  | refl  : ∀ (x : α), EqvGen r x x
+  | symm  : ∀ (x y : α), EqvGen r x y → EqvGen r y x
+  | trans : ∀ (x y z : α), EqvGen r x y → EqvGen r y z → EqvGen r x z
+
+def eqv_lift {α : Type} (r : α → α → Prop) (x : α) : Quot r → Prop :=
+  Quot.lift (fun y => EqvGen r x y) (by
+    intro y1 y2 hr
+    apply propext
+    constructor
+    · intro h
+      exact EqvGen.trans x y1 y2 h (EqvGen.rel y1 y2 hr)
+    · intro h
+      exact EqvGen.trans x y2 y1 h (EqvGen.symm y1 y2 (EqvGen.rel y1 y2 hr))
+  )
+
+theorem quot_exact {α : Type} {r : α → α → Prop} {x y : α} (h : Quot.mk r x = Quot.mk r y) : EqvGen r x y := by
+  have h_lift : eqv_lift r x (Quot.mk r x) = eqv_lift r x (Quot.mk r y) := by rw [h]
+  have h_refl : eqv_lift r x (Quot.mk r x) := EqvGen.refl x
+  rw [h_lift] at h_refl
+  exact h_refl
+
+-- Núcleo de capacidad K_D.
+def K_D (x y : S) : Prop :=
+  ∀ c, C_D c → K c x y
+
+-- Axioma 0: Contract Coherence.
+def Axiom0 : Prop :=
+  ∀ x y, kernel D x y ↔ K_D K C_D x y
+
+-- T1: Caracterización de la suficiencia.
+theorem T1_characterization (R : S → Z) (hA0 : Axiom0 K C_D D) :
+    kernelSubset R D ↔ (∀ x y, kernel R x y → K_D K C_D x y) := by
+  constructor
+  · intro h x y hker
+    have hD := h x y hker
+    exact (hA0 x y).mp hD
+  · intro h x y hker
+    have hKD := h x y hker
+    exact (hA0 x y).mpr hKD
+
+-- T2 (Upset): Si R1 es suficiente y R1 sqsubseteq R2 (es decir, ker(R2) ⊆ ker(R1)), R2 es suficiente.
+theorem T2_upset {Z1 Z2 : Type} (R1 : S → Z1) (R2 : S → Z2) (h_sufficient : kernelSubset R1 D) (h_refine : kernelSubset R2 R1) :
+    kernelSubset R2 D := by
+  intro x y hker
+  exact h_sufficient x y (h_refine x y hker)
+
+-- Demostración de que K_D es relación de equivalencia.
+theorem K_D_refl (hK_equiv : ∀ c, Equivalence (K c)) (x : S) : K_D K C_D x x := by
+  intro c hCD
+  exact (hK_equiv c).refl x
+
+theorem K_D_symm (hK_equiv : ∀ c, Equivalence (K c)) {x y : S} (h : K_D K C_D x y) : K_D K C_D y x := by
+  intro c hCD
+  exact (hK_equiv c).symm (h c hCD)
+
+theorem K_D_trans (hK_equiv : ∀ c, Equivalence (K c)) {x y z : S} (h1 : K_D K C_D x y) (h2 : K_D K C_D y z) : K_D K C_D x z := by
+  intro c hCD
+  exact (hK_equiv c).trans (h1 c hCD) (h2 c hCD)
+
+theorem K_D_equivalence (hK_equiv : ∀ c, Equivalence (K c)) : Equivalence (K_D K C_D) :=
+  ⟨K_D_refl K C_D hK_equiv, K_D_symm K C_D hK_equiv, K_D_trans K C_D hK_equiv⟩
+
+-- Helper lemma to project EqvGen to K_D when it is an equivalence relation.
+theorem eqvGen_of_equiv {α : Type} {r : α → α → Prop} (heqv : Equivalence r) {x y : α} (h : EqvGen r x y) : r x y := by
+  induction h with
+  | rel a b hr => exact hr
+  | refl a => exact heqv.refl a
+  | symm a b _ ih => exact heqv.symm ih
+  | trans a b c _ _ ih1 ih2 => exact heqv.trans ih1 ih2
+
+-- R_min cociente
+def R_min (_hK_equiv : ∀ c, Equivalence (K c)) (s : S) : Quot (K_D K C_D) :=
+  Quot.mk (K_D K C_D) s
+
+theorem kernel_R_min_eq_K_D (hK_equiv : ∀ c, Equivalence (K c)) (x y : S) :
+    kernel (R_min K C_D hK_equiv) x y ↔ K_D K C_D x y := by
+  constructor
+  · intro h
+    dsimp [R_min, kernel] at h
+    have hg := quot_exact h
+    have heqv : Equivalence (K_D K C_D) := K_D_equivalence K C_D hK_equiv
+    exact eqvGen_of_equiv heqv hg
+  · intro h
+    dsimp [R_min, kernel]
+    exact Quot.sound h
+
+-- R_min es suficiente.
+theorem R_min_sufficient (hK_equiv : ∀ c, Equivalence (K c)) (hA0 : Axiom0 K C_D D) :
+    kernelSubset (R_min K C_D hK_equiv) D := by
+  intro x y hker
+  have hKD : K_D K C_D x y := (kernel_R_min_eq_K_D K C_D hK_equiv x y).mp hker
+  exact (hA0 x y).mpr hKD
+
+-- R_min es el mínimo de las representaciones suficientes.
+theorem R_min_is_minimum (hK_equiv : ∀ c, Equivalence (K c)) (hA0 : Axiom0 K C_D D)
+    (R : S → Z) (h_sufficient : kernelSubset R D) :
+    kernelSubset R (R_min K C_D hK_equiv) := by
+  intro x y hker
+  have hKD : K_D K C_D x y := (T1_characterization K C_D D R hA0).mp h_sufficient x y hker
+  exact (kernel_R_min_eq_K_D K C_D hK_equiv x y).mpr hKD
+
+end StructuralSufficiency
